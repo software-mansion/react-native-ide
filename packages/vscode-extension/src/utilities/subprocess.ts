74a79cb2be06c8188247db0ac071c744a2c5cc1f
Jakub Gonet <jakub.gonet@swmansion.com>
2024-07-10 15:44:19 +0200

    Simplify arguments usage

--- packages/vscode-extension/src/utilities/subprocess.ts
+++ packages/vscode-extension/src/utilities/subprocess.ts
@@ -4,14 +4,11 @@ import readline from "readline";
 
 export type ChildProcess = ExecaChildProcess<string>;
 
-function updatePWDInArgs<T extends execa.Options>(args: [string, string[]?, T?]) {
-  if (args.length > 1) {
-    const options = args[args.length - 1] as T;
-    if (options && options.cwd) {
-      (args[args.length - 1] as T) = { ...options, env: { ...options.env, PWD: "0" } };
-    }
+function overridePWD<T extends execa.Options>(options?: T) {
+  if (options?.cwd) {
+    return { ...options, env: { ...options.env, PWD: "0" } };
   }
-  return args;
+  return undefined;
 }
 
 /**
@@ -42,37 +39,24 @@ export function lineReader(childProcess: ExecaChildProcess<string>, includeStder
   };
 }
 
-export function exec(
-  ...args: [string, string[]?, (execa.Options & { allowNonZeroExit?: boolean })?]
-) {
-  const subprocess = execa(...updatePWDInArgs(args));
-  const allowNonZeroExit = args[2]?.allowNonZeroExit;
+type ExecArgs = [string, string[]?, (execa.Options & { allowNonZeroExit?: boolean })?];
+export function exec([name, args, options]: ExecArgs) {
+  const subprocess = execa(name, args, overridePWD(options));
+  const allowNonZeroExit = options?.allowNonZeroExit;
   async function printErrorsOnExit() {
     try {
       const result = await subprocess;
       if (result.stderr) {
-        Logger.debug(
-          "Subprocess",
-          args[0],
-          args[1]?.join(" "),
-          "produced error output:",
-          result.stderr
-        );
+        Logger.debug("Subprocess", name, args?.join(" "), "produced error output:", result.stderr);
       }
     } catch (e) {
       // @ts-ignore idk how to deal with error objects in ts
       const { exitCode, signal } = e;
       if (exitCode === undefined && signal !== undefined) {
-        Logger.info("Subprocess", args[0], "was terminated with", signal);
+        Logger.info("Subprocess", name, "was terminated with", signal);
       } else {
         if (!allowNonZeroExit || !exitCode) {
-          Logger.error(
-            "Subprocess",
-            args[0],
-            args[1]?.join(" "),
-            "execution resulted in an error:",
-            e
-          );
+          Logger.error("Subprocess", name, args?.join(" "), "execution resulted in an error:", e);
         }
       }
     }
@@ -81,30 +65,24 @@ export function exec(
   return subprocess;
 }
 
-export function execSync(...args: [string, string[]?, execa.SyncOptions?]) {
-  const result = execa.sync(...updatePWDInArgs(args));
+export function execSync([name, args, options]: [string, string[]?, execa.SyncOptions?]) {
+  const result = execa.sync(name, args, overridePWD(options));
   if (result.stderr) {
-    Logger.debug(
-      "Subprocess",
-      args[0],
-      args[1]?.join(" "),
-      "produced error output:",
-      result.stderr
-    );
+    Logger.debug("Subprocess", name, args?.join(" "), "produced error output:", result.stderr);
   }
   return result;
 }
 
-export function command(...args: [string, execa.Options?]) {
-  const subprocess = execa.command(...updatePWDInArgs(args));
+export function command([commandWithArgs, options]: [string, execa.Options?]) {
+  const subprocess = execa.command(commandWithArgs, overridePWD(options));
   async function printErrorsOnExit() {
     try {
       const result = await subprocess;
       if (result.stderr) {
-        Logger.debug("Command", args[0], "produced error output:", result.stderr);
+        Logger.debug("Command", commandWithArgs, "produced error output:", result.stderr);
       }
     } catch (e) {
-      Logger.error("Command", args[0], "execution resulted in an error:", e);
+      Logger.error("Command", commandWithArgs, "execution resulted in an error:", e);
     }
   }
   printErrorsOnExit(); // don't want to await here not to block the outer method
